88e29b75aedfed5f413b3418dc0ad59bbec91278
Arlan S <36273736+ArlanSul@users.noreply.github.com>
2026-03-10 22:01:14 -0400

    Checkpoint 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
+import samosa from './assets/samosa.png'
 
 const App = () => {
 
   return (
     <div className="App">
-
+      <div className="header">
+        <h1>Somosa Selector</h1>
+        <h2>Count:</h2>
+        <img src={samosa} className="samosa" />
+      </div>
     </div>
   )
 }
